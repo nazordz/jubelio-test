df1606eeed36f2523a369ad412a8fd135dcad00e
Naufal <nazor.dz@gmail.com>
2023-06-17 20:23:10 +0700

    switch to connection string

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -3,12 +3,12 @@ import { Pool } from "pg";
 
 dotenv.config();
 const db = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT!),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-
+    // host: process.env.DB_HOST,
+    // port: Number(process.env.DB_PORT!),
+    // database: process.env.DB_NAME,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?currentSchema=${process.env.DB_SCHEMA}`
 });
 
 db.on('error', (err, client) => {
